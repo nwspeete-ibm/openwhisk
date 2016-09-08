720ff720a5c048191ab2ac8ac0ae2271e4687287
Mark Deuser <mdeuser@us.ibm.com>
2016-09-08 17:04:27 -0400

    Restify "hello" endpoint in build

---
+++ core/restify/src/main/scala/whisk/core/restify/Restify.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package whisk.core.restify
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+import scala.io.StdIn
+
+object Restify {
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("restify-actor-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("hello") {
+        get {
+          //complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "Hello, World"))
+          complete("Hello, World")
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8077/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+    //FIXME MWD  .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
+
+
